27996786ef43b12ae7f889f1330b48699d1f1f70
jairo <jairo.cab17@gmail.com>
2018-05-04 11:51:16 -0300

    se modifica web scraping

--- scrapping/scrapping.rb
+++ scrapping/scrapping.rb
@@ -10,7 +10,7 @@ url = 'http://www.reactiongifs.com/'
   div_main.css('div.post').each do |post|
     title = post.css('h2').text
     gif_url = post.css('div.entry img').attr('src')
-    File.open("gifs/#{title}", 'w') do |new_file|
+    File.open("./gifs/#{title}", 'w') do |new_file|
       puts "Descargando gif: #{title}"
       open(gif_url, 'r') do |gif|
         new_file.write(gif.read)
